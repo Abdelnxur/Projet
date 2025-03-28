18e1c5b0625577df1c42e9049600dd25ab3cf49d
Abdelnxur <Abdelnour.Alem@etu.univ-smb.fr>
2025-03-28 17:42:35 +0100

    Update connexion.php

--- include/connexion.php
+++ include/connexion.php
@@ -1,7 +1,7 @@
 <?php
 require('connect.php');
 
-$CONNEXION = mysqli_connect(SERVEUR_BD, LOGIN_BD, PASS_BD);
+$CONNEXION = mysqli_connect(SERVEUR_BD, USER_BD, PASS_BD);
 
 if (!$CONNEXION) {
     die("Erreur de connexion au serveur : " . mysqli_connect_error());
@@ -14,5 +14,4 @@ if (!mysqli_select_db($CONNEXION, NOM_BD)) {
 if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
     die("Erreur encodage UTF-8 : " . mysqli_error($CONNEXION));
 }
-
 ?>
