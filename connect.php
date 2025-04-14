8d593ec0f74005fff8a93f2df020700b2ba7ed9c
Abdelnxur <Abdelnour.Alem@etu.univ-smb.fr>
2025-04-14 09:17:22 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,4 +3,15 @@ define('SERVEUR_BD', getenv('SERVEUR_BD'));
 define('USER_BD', getenv('MARIADB_USER'));
 define('PASS_BD', getenv('MARIADB_PASSWORD'));
 define('NOM_BD', getenv('MARIADB_DATABASE'));
+
+$CONNEXION = mysqli_connect(SERVEUR_BD, USER_BD, PASS_BD);
+if (!$CONNEXION) {
+    die("Erreur de connexion au serveur : " . mysqli_connect_error());
+}
+if (!mysqli_select_db($CONNEXION, NOM_BD)) {
+    die("Erreur de sélection de la base : " . mysqli_error($CONNEXION));
+}
+if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
+    die("Erreur encodage UTF-8 : " . mysqli_error($CONNEXION));
+}
 ?>
