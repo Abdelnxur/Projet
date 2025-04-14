46fe01aa87700945f0f10d005d47de167247951c
Abdelnxur <Abdelnour.Alem@etu.univ-smb.fr>
2025-04-14 08:38:03 +0200

    Delete include/connexion.php

--- include/connexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SERVEUR_BD', getenv('SERVEUR_BD'));
-define('USER_BD', getenv('MARIADB_USER'));
-define('PASS_BD', getenv('MARIADB_PASSWORD'));
-define('NOM_BD', getenv('MARIADB_DATABASE'));
-?>
